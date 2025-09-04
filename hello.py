1fa84a8f8137167d854a83bf6f3c2f3c76804635
miko89pro <nurrahmatjatmiko@gmail.com>
2025-09-04 22:51:17 +0700

    tes

---
+++ hello.py
@@ -0,0 +1,27 @@
+print ('Hello World')
+
+#namastring
+
+nama = 'miko'
+
+
+#angkastring
+umur = 20
+
+print (umur)
+
+#angkafloat
+a = 2.5
+b = 1.5
+c = a+b 
+
+print(c)
+
+#boolean
+status = False
+
+statusmenikah = 'Sudah Kawin' if status else 'Belum Kawin'
+
+print(f"statusmenikah: {'Sudah Kawin' if status else 'Belum Kawin'}")
+
+print(statusmenikah)
